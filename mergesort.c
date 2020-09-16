795fdf848776850c4784640729ec0c57db690f66
ybuchy <yann.buchy@gmx.de>
2020-09-16 20:14:11 +0200

    +update_merged function and tried out mmap / fork for speed increase

--- mergesort.c
+++ mergesort.c
@@ -1,5 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/mman.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/wait.h>
+
+void update_merged(int *merged, int *cur_ind, const int *half, int *half_ind) {
+    merged[*cur_ind] = half[*half_ind];
+    (*cur_ind)++;
+    (*half_ind)++;
+}
 
 int *merge(const int fst_half[], const int fst_len,
 	   const int snd_half[], const int snd_len) {
@@ -10,47 +21,62 @@ int *merge(const int fst_half[], const int fst_len,
     while (fst_ind < fst_len || snd_ind < snd_len) {
 	if (fst_ind == fst_len) {
 	    for (int i = snd_ind; i < snd_len; ++i) {
-		merged[cur_ind] = snd_half[snd_ind];
-		cur_ind++;
-		snd_ind++;
+		update_merged(merged, &cur_ind, snd_half, &snd_ind);
 	    }
 	} else if (snd_ind == snd_len) {
 	    for (int i = fst_ind; i < fst_len; ++i) {
-		merged[cur_ind] = fst_half[fst_ind];
-		cur_ind++;
-		fst_ind++;
+		update_merged(merged, &cur_ind, fst_half, &fst_ind);
 	    }
 	} else if (fst_half[fst_ind] <= snd_half[snd_ind]) {
-	    merged[cur_ind] = fst_half[fst_ind];
-	    cur_ind++;
-	    fst_ind++;
+	    update_merged(merged, &cur_ind, fst_half, &fst_ind);
 	} else {
-	    merged[cur_ind] = snd_half[snd_ind];
-	    cur_ind++;
-	    snd_ind++;
+	    update_merged(merged, &cur_ind, snd_half, &snd_ind);
 	}
     }
     return merged;
 }
 
-int *merge_sort(int arr[], const int len) {
+int FORK_COUNTER = 0;
+
+void merge_sort(int arr[], const int len) {
     if (len == 1) {
-	return arr;
+	return;
     } else {
 	const int mid = len / 2;
 	const int len_fst = len - mid;
 	const int len_snd = mid;
-	int *fst_half = merge_sort(arr, len_fst);
-	int *snd_half = merge_sort(arr+len_fst, len_snd);
-	return merge(fst_half, len_fst, snd_half, len_snd);
+	// If forked too often, it gets way too slow. (limited) testing showed that 5 is one of the fastest
+	if (FORK_COUNTER < 5) {
+	    pid_t pid = fork();
+	    FORK_COUNTER++;
+	    if (pid == 0) { // Child process
+		merge_sort(arr, len_fst);
+		exit(0);
+	    } else { // Parent process
+		merge_sort(arr+len_fst, len_snd);
+		waitpid(pid, NULL, 0);
+	    }
+	} else {
+	    merge_sort(arr, len_fst);
+	    merge_sort(arr+len_fst, len_snd);
+	}
+
+	memcpy(arr, merge(arr, len_fst, arr+len_fst, len_snd),
+	       len * sizeof(int));
     }
 }
 
 int main(int argc, char **argv) {
-    int arr[] = {2, 15, 91, 2, 23, 54, 125, 21, 52};
-    int *sorted = merge_sort(arr, 9);
-    for (int i = 0; i < 9; ++i) {
-	printf("%d\n", sorted[i]);
+    // int arr[] = {2, 15, 91, 2, 23, 54, 125, 21, 52};
+    // create file descriptor for mmap
+    int *arr = mmap(NULL, 1000000 * sizeof(int), PROT_READ | PROT_WRITE,
+		      MAP_SHARED | MAP_ANONYMOUS, 0, 0);
+    for (int i = 0; i < 1000000; ++i) {
+	arr[i] = 1284123 - i;
+    }
+    merge_sort(arr, 1000000);
+    for (int i = 0; i < 10; ++i) {
+	printf("%d\n", arr[i]);
     }
     return 0;
 }
